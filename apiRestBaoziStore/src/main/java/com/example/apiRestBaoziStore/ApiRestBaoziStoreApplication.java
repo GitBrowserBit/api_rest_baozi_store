aea198aa10aa179d3cb698408953c3654f2eeee7
GitBrowserBit <browsergit@gmail.com>
2026-03-21 15:32:08 -0300

    commit inicial

---
+++ apiRestBaoziStore/src/main/java/com/example/apiRestBaoziStore/ApiRestBaoziStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiRestBaoziStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBaoziStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBaoziStoreApplication.class, args);
+	}
+
+}
---
+++ apiRestBaoziStore/src/test/java/com/example/apiRestBaoziStore/ApiRestBaoziStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiRestBaoziStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBaoziStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
